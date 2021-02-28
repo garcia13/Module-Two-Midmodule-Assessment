7fbfca905973587a80f471efb938fe1b3972ecd6
Miguel Garcia <miguelgarcia@pursuit.org>
2021-02-28 14:41:57 -0500

    complete with css

--- index.js
+++ index.js
@@ -11,29 +11,31 @@ const numberOfDice = document.querySelector("#number-of-dice");
 const rollResult = document.querySelector("#dice-para");
 const dieSum = document.querySelector("#sum-para");
 const ul = document.querySelector("#history-list");
-const form = document.querySelector("form")
-let newRollResult = ""
+const form = document.querySelector("form");
+let newRollResult = "";
+let oldResult = "test";
+let counter = 0;
 
 const diceRoller = (event) => {
     event.preventDefault();
     rollResult.textContent = "";
     let num = numberOfDice.valueAsNumber;
-    let sum = 0
+    let sum = 0;
     for (let i = 0; i < num; i++) {
         const diceIndex = Math.floor(Math.random() * dice.length);
         rollResult.textContent += String.fromCodePoint(dice[diceIndex]);
-        sum += diceIndex
+        sum += diceIndex;
     }
     dieSum.textContent = `Sum: ${sum + num}`;
-    
-    let oldRollResult = `${rollResult.textContent} = ${sum + num}`;
-    if (newRollResult === "") {
-        newRollResult.textContent = oldRollResult.textContent;
-        
-    const li = document.createElement("li");
-    li.textContent = oldRollResult;
-    ul.appendChild(li);
+    counter++;
+    newRollResult = `${rollResult.textContent} = ${sum + num}`
+    if (counter > 1) {        
+        const li = document.createElement("li");
+        li.textContent = oldResult;
+        ul.appendChild(li);
     }
+    oldResult = newRollResult
+    newRollResult = ""
 }
 
 
